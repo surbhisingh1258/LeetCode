e9216c2664b612bac25655246df8b2c2bc69e6e7
surbhisingh1258 <surbhisingh1258@gmail.com>
2025-08-20 22:06:46 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -0,0 +1,14 @@
+// Last updated: 8/20/2025, 10:06:45 PM
+import java.util.Arrays;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+        
+        return Arrays.equals(sChars, tChars);
+    }
+}
