120e27d53ce2c2ccaca32d7114b254a1d1dd29ac
surbhisingh1258 <surbhisingh1258@gmail.com>
2025-08-20 22:05:31 +0530

    Create 1448-Maximum69Number

---
+++ 1448-Maximum69Number/1448-Maximum69Number.java
@@ -0,0 +1,6 @@
+// Last updated: 8/20/2025, 10:05:30 PM
+class Solution {
+    public int maximum69Number (int num) {
+        return Integer.parseInt(String.valueOf(num).replaceFirst("6","9"));
+    }
+}
